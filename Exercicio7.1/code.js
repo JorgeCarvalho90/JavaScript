b7277a703c4e9fe00af18f072bbb406549de3849
JorgeCarvalho90 <jorge_carvalho_17@hotmail.com>
2025-02-03 19:03:57 +0000

    30/01

--- Exercicio7.1/code.js
+++ Exercicio7.1/code.js
@@ -12,8 +12,10 @@ async function fetchAll() {
   const orders = await fetchOrders()
   const products = await fetchProducts()
   renderAll(orders, products)
+  formatForm(orders, products)
   //   callPrices(products)
 }
+
 
 const conversionRates = {
   EUR: 1,
@@ -30,21 +32,30 @@ function convertPrice(amount, fromCurrency, toCurrency) {
 }
 
 const ordersTable = document.querySelector("tbody")
+const myFooter = document.querySelector("footer")
+const productsForm = document.querySelector("#products_select")
+const submitButton = document.querySelector("#submit_button")
 fetchAll()
 
 function renderAll(calledOrder, calledProducts) {
   ordersTable.innerHTML = ""
 
+  let calculateAllEUR = 0
+  let calculateAllUSD = 0
+  let calculateAllGBP = 0
+
   for (const i of calledOrder) {
     const newLine = document.createElement("tr")
+
+    const addIds = document.createElement("td")
+    addIds.innerText = i.id
+    newLine.appendChild(addIds)
+
 
     const addCurrency = document.createElement("td")
     addCurrency.innerText = i.currency
     newLine.appendChild(addCurrency)
 
-    const addIds = document.createElement("td")
-    addIds.innerText = i.id
-    newLine.appendChild(addIds)
 
     const addStatus = document.createElement("td")
     addStatus.innerText = i.status
@@ -99,13 +110,64 @@ function renderAll(calledOrder, calledProducts) {
     if (i.currency === "EUR") {
       addTotalValue.innerText =
         "Total Value = " + totalOrderValue.toFixed(2) + " " + i.currency
+      calculateAllEUR += totalOrderValue
     } else {
       const conversionMade = convertPrice(totalOrderValue, "EUR", i.currency)
-      addTotalValue.innerText ="Conversion from " + totalOrderValue + "EUR = " + conversionMade.toFixed(2) + " " + i.currency
+      addTotalValue.innerText =
+        "Conversion from " +
+        totalOrderValue +
+        "EUR = " +
+        conversionMade.toFixed(2) +
+        " " +
+        i.currency
+      if (i.currency === "USD") {
+        calculateAllUSD += conversionMade
+      } else {
+        calculateAllGBP += conversionMade
+      }
     }
-
     newLine.appendChild(addTotalValue)
-
     ordersTable.appendChild(newLine)
   }
+
+  const addAllValue = document.createElement("span")
+  addAllValue.innerHTML =
+    "Total Revenue:<p></p> " +
+    calculateAllEUR.toFixed(2) +
+    "<b>EUR</b> <p></p> " +
+    calculateAllUSD.toFixed(2) +
+    "<b>USD</b> <p></p> " +
+    calculateAllGBP.toFixed(2) +
+    "<b>GBP</b>"
+  myFooter.appendChild(addAllValue)
 }
+
+function formatForm(calledOrder, calledProducts){
+  for (const i of calledProducts){
+    const addProducts = document.createElement("option")
+    addProducts.value = i.name
+    addProducts.innerText = i.name
+    productsForm.appendChild(addProducts)
+  }
+}
+
+submitButton.addEventListener('click',(event)=>{
+
+  const myPayload = JSON.stringify({
+    "currency": "string",
+    "products": [
+      {
+        "name": "string",
+        "quantity": 0
+      }
+    ]
+  })
+
+  fetch("http://116.203.151.6:3000/orders", {
+    method:"POST",
+    headers: {"Content-Type": "application/json"},
+    body: myPayload
+  })
+
+
+})
